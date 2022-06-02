79010444216708b1edecd6a0f3bafbe22110d017
zoeurk <zoeurk@gmail.com>
2022-06-02 19:53:19 +0200

    update

--- arithmetique/arithmetique.c
+++ arithmetique/arithmetique.c
@@ -377,11 +377,16 @@ void *puissance(void *num1, void *num2, unsigned long int internal_buflen, char 
 		return n_;\
 	}\
 	memset(buffer, 0, internal_buflen);\
-	for(n = n, i = 1;i * 2 > i && equal(n, "1000") > 0;n_ = racine_carree(n, virgule, approximation), free(n), n = n_, i *= 2);;\
-	if(i*2 < i){\
+	for(n = n, i = 1;i * 2 > i && equal(n, "10") > 0;n_ = racine_carree(n, virgule, approximation), free(n), n = n_, i *= 2);;\
+	/*if(i*2 < i){\
 		fprintf(stderr, "Nombre trop long pour %s.\n", msg);\
 		exit(EXIT_FAILURE);\
-	}\
+	}*/\
+	for(n = n, i = 1;i / 2 > i && equal(n, "0.000001") > 0;n_ = puissance(n, "2", internal_buflen,"%.16Lf",16,1), free(n), n = n_, i /= 2);;\
+	/*if(i/2 < i){\
+		fprintf(stderr, "Nombre trop long pour %s.\n", msg);\
+		exit(EXIT_FAILURE);\
+	}*/\
 	result = i*fn(strtold(n, NULL));\
 	snprintf(buffer, internal_buflen, format, result);\
 	if(buffer[internal_buflen-1] != 0){\
