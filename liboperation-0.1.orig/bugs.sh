7adb0d8bcd795447b992aa223c9118a5b819c0d1
zoeurk <zoeurk@gmail.com>
2025-11-06 11:50:17 +0100

    a_travailler

---
+++ liboperation-0.1.orig/bugs.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+LD_LIBRARY_PATH='./operation/;./arithmetique/' ./main-bin 220737010757610437170977.1196154726482032501716639 602532997634851606012054.02476539253 2 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 0.01864652475776302738994 0.280 13 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 0.032501716639 602532997634851606012054.02476539253 2 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 0.8991836423285267414358 0.428351262684061951 10 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 113344014648661020828564.775646601052872510872053 988220035095750362401769.0583199816891737239 13 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 123456789.1 602532997634851606012054.02476539253 2 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 173113603418020904859998.30098420691233691920 73502708476386663614862.213824673 2 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 200808728160524910353246.1560997940032043945 748165102616922255283435.583566033417257953765 16 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 220737010757610437170977.1196154726482032501716639 602532997634851606012054.02476539253 2 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 220737010757610437170977.1196154726482032501716639 602532997634851606012054.02476539253 2 2>dump.dbg || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 220737010757610437170977.1196154726482032501716639 602532997634851606012054.02476539253 2 > dump.txt  || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 220737010757610437170977.1196154726482032501716639 602532997634851606012054.02476539253 2 > dump.txt 2>&1 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 220737010757610437170977.1196154726482032501716639 602532997634851606012054.02476539253 2~k,iju || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 294712748912794689860379.654932478828107 488471808957045853360799.08427557793545 0 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 32501716639 602532997634851606012054.02476539253 2 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 412985427634088654917219.3932249942778667 250476844434271763179979.9 6 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 48621347371633478294041303327992675669489585710000000000000 8188601510643167772945745988555733577477683680476 0 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 486213473716334782940413.0332799267566948958571 818860151064316777294574.5988555733577477683680476 2 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 507484550240329594872968.13522545204852 648645761806668192568855.68 2 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 590691996643015182726786.622247 764736400396734569314106.201007095445181963836118 9 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 590691996643015182726786.622247 764736400396734569314106.201007095445181963836118 9 > tst1.txt || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 591439688715953307392995.51 558068207827878233005263.8044861524376287479972 23 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 654779888609140154116120.50185397116044861 572838941023880369268329.32721 3 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 654779888609140154116120.50185397116044861 572838941023880369268329.32721 3 > tst2.txt  || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 671641107804989700160219.0582 447409781033035782406347.305699244678416113527122 23 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 72632944228274967574578.3968871595330739299 427542534523537041275653 17 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 72831311512932021057450.1446097505149 932906080720225833524070.17413 1 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 780651560234988937209124.1144426642252231631952391 42175936522468909742885.438864728770885 7 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 818326085297932402532996.123456789816525520714122224765 974822613870450904097046.4044403753719 3 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 818326085297932402532996.816525520714122224765 974822613870450904097046.1234567894044403753719 3 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 818326085297932402532996.816525520714122224765 974822613870450904097046.4044403753719 3 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 847791256580453192950331.03593499656 736537727931639581902799.2939803 2 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 9.123456789 602532997634851606012054.02476539253 2 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 9.1 602532997634851606012054.02476539253 2 || exit
+LD_LIBRARY_PATH='./operation/;./arithmetique/' valgrind --leak-check=full ./main-bin 9.9 602532997634851606012054.02476539253 2 || exit
