dd8834fff38491748daec086a111a120194d3887
zoeurk <zoeurk@gmail.com>
2026-01-09 13:42:00 +0100

    update

--- liboperation-0.1.orig/main/main.c
+++ liboperation-0.1.orig/main/main.c
@@ -137,6 +137,7 @@ int main(int argc, char **argv){
 	}else
 		printf("NULL\n");
 	destroy_nbr(result);
+	fprintf(stderr, "Method of Kuhn\n");
 	res = kdivision(nbr1, nbr2, bvirg, virg);
 	destroy_nbr(res);
 	destroy_nbr(nbr1);
