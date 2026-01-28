ab238678f9d2aa3a007432ed4192ddc5d0d57c8d
zoeurk <zoeurk@gmail.com>
2026-01-28 17:51:56 +0100

    update

--- liboperation-0.1.orig/main/main.c
+++ liboperation-0.1.orig/main/main.c
@@ -128,8 +128,8 @@ int main(int argc, char **argv){
 		printf("NULL\n");
 	for(i = DIVISEUR; i <= MODULO;i++)
 		destroy_nbr(ediv_spaces[i]);*/
-	fprintf(stderr, "Method of Newton-Raphston (Not Really Tested)\n");
-	/*if((res = nrdivision(nbr1, nbr2, bvirg, virg, NULL)) != NULL){
+	/*fprintf(stderr, "Method of Newton-Raphston (Not Really Tested)\n");
+	if((res = nrdivision(nbr1, nbr2, bvirg, virg, NULL)) != NULL){
 		printf("%s / %s = ", argv[1], argv[2]);
 		print_nbr(res);
 		putchar('\n');
